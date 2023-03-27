95d9b2942f98ff3df20442aab4f5adb63fbbee58
Yarik-33 <yarik.kuchinskiy@gmail.com>
2023-03-27 19:25:20 +0300

    added description of readme.md file

--- src/main/java/tms/Vagazzi/Main.java
+++
@@ -1,7 +0,0 @@
-package tms.Vagazzi;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/main/java/tms/Vagazzi/ThirdLessonClasswork.java
@@ -0,0 +1,12 @@
+package tms.Vagazzi;
+
+public class ThirdLessonClasswork {
+    public static void main(String[] args) {
+        int a = 10;
+        ++a;
+        int b = 20;
+        b--;
+
+        System.out.println(a<b?"A is less than b":"A is not less than b");
+    }
+}
