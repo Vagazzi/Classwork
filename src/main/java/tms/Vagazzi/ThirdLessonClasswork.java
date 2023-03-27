9bbb51c37743c5e8a5f4efaab616118236549cf5
Yarik-33 <yarik.kuchinskiy@gmail.com>
2023-03-27 19:37:04 +0300

    added some logic into the ThirdLessonClasswork class

--- src/main/java/tms/Vagazzi/ThirdLessonClasswork.java
+++ src/main/java/tms/Vagazzi/ThirdLessonClasswork.java
@@ -1,12 +1,18 @@
 package tms.Vagazzi;
 
+import java.util.Scanner;
+
 public class ThirdLessonClasswork {
     public static void main(String[] args) {
-        int a = 10;
-        ++a;
-        int b = 20;
-        b--;
 
-        System.out.println(a<b?"A is less than b":"A is not less than b");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Set a number 'a' ");
+        int a = scanner.nextInt();
+
+        System.out.println("Set a number 'b' ");
+        int b = scanner.nextInt();
+
+        System.out.println(a < b ? "A is less than b" : "A is not less than b");
     }
 }
